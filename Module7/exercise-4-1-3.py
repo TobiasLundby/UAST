447ce19edd5bd6c5d00341dff2ff0405c321a274
Tobias Lundby <tobiaslundby@gmail.com>
2017-03-29 21:30:47 +0200

    final of day

--- Module7/exercise-4-1-3.py
+++ Module7/exercise-4-1-3.py
@@ -13,3 +13,8 @@ data = Route_Plan_obj.get_pos_4DOF_nmea()
 # Print first 10 values to test functionality
 for i in range(10):
     print data[i]
+
+data_full = Route_Plan_obj.get_nmea_data()
+# Generate KML file for Google Maps. Simpsle viewer on http://www.gpsvisualizer.com/
+data_full.generate_track_file('drone_track.kml','Drone track','This is the track of the drone',1)
+# wierdly there is no need for including the drone_track, but I think it is a dependency trhough route_plan_class->nmea_parser_class->exportkml
