753ab8049e0a1c20ae9026e812e4b9a0efb8ce89
Tobias Lundby <tobiaslundby@gmail.com>
2017-03-29 21:26:48 +0200

    small change

--- Module7/exercise-4-1-3.py
+++ Module7/exercise-4-1-3.py
@@ -1,31 +1,3 @@
-#!/usr/bin/python
-#/****************************************************************************
-# exercise-4-1-3
-# Copyright (c) 2016, Tobias Lundby <tobiaslundby@gmail.com>
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#    * Redistributions of source code must retain the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#    * Redistributions in binary form must reproduce the above copyright
-#      notice, this list of conditions and the following disclaimer in the
-#      documentation and/or other materials provided with the distribution.
-#    * Neither the name of the copyright holder nor the names of its
-#      contributors may be used to endorse or promote products derived from
-#      this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL HENRIK EGEMOSE SCHMIDT BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#****************************************************************************/
 import sys
 from route_plan_class import Route_Plan  # Import NMEA parser
 
@@ -36,7 +8,7 @@ Route_Plan_obj = Route_Plan(nmea_file, aq_log_file)
 
 data = Route_Plan_obj.get_pos_4DOF_nmea()
 
-Route_Plan_obj.print_aq_info()
+#Route_Plan_obj.print_aq_info()
 
 # Print first 10 values to test functionality
 for i in range(10):
